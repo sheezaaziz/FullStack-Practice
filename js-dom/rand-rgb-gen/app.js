d1c50d69f9f8c04f5b94ff2e48a35f7e5e32a640
Sheeza Aziz <sheeza.aziz@utoronto.ca>
2020-12-24 23:09:18 -0500

    fixed rgb-rand-col to be modular

--- js-dom/rand-rgb-gen/app.js
+++ js-dom/rand-rgb-gen/app.js
@@ -3,13 +3,16 @@ const btn = document.querySelector('button');
 const body = document.querySelector('body');
 const rgbText = document.querySelector('#rgb-text');
 
-btn.addEventListener('click', () => {
-  // Create random body background colour.
+const makeRandColor = () => {
   r = Math.floor(Math.random() * 256);
   g = Math.floor(Math.random() * 256);
   b = Math.floor(Math.random() * 256);
   rgb = `rgb(${r}, ${g}, ${b})`;
+  return rgb;
+}
 
+btn.addEventListener('click', () => {
+  rgb = makeRandColor();
   // Change body background color + text.
   body.style.background = rgb;
   rgbText.innerText = rgb;
