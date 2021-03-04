3c18cfc84b965407813fc3506dd22c0b5f917215
Sheeza Aziz <sheeza.aziz@utoronto.ca>
2021-03-03 20:53:09 -0500

    [yelpcamp]: E, campground

--- yelpcamp/app.js
+++ yelpcamp/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const path = require('path');
+const methodOverride = require('method-override');
 
 const Campground = require('./models/campground');
 
@@ -19,6 +20,7 @@ mongoose.connect('mongodb://localhost:27017/yelpcamp', {
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(methodOverride('_method'));
 // to parse body in req.body
 app.use(express.urlencoded({extended: true}));
 
@@ -44,6 +46,17 @@ app.post('/campgrounds', async (req, res) => {
 app.get('/campgrounds/:id', async (req, res) => {
   const campground = await Campground.findById(req.params.id);
   res.render('campgrounds/show', { campground });
+})
+
+app.get('/campgrounds/:id/edit', async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  res.render('campgrounds/edit', { campground });
+})
+
+app.put('/campgrounds/:id', async (req, res) => {
+  const { id } = req.params;
+  const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+  res.redirect(`/campgrounds/${campground._id}`);
 })
 
 app.listen(3000, () => {
