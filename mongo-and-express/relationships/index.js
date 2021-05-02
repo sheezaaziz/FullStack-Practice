a80280d2996a21472745d047fdae5cfe9fba033a
Sheeza Aziz <sheeza.aziz@utoronto.ca>
2021-05-02 14:39:15 -0400

    [mongo-and-express/relationships] implemented

--- mongo-and-express/relationships/index.js
+++ mongo-and-express/relationships/index.js
@@ -3,6 +3,8 @@ const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
+
+const Farm = require('./models/farm');
 const Product = require('./models/product');
 
 mongoose.connect('mongodb://localhost:27017/farmStand', {useNewUrlParser: true, useUnifiedTopology: true})
@@ -20,6 +22,51 @@ app.use(express.urlencoded({extended: true}));
 // override with POST having ?_method=DELETE
 app.use(methodOverride('_method'));
 
+// FARM ROUTES
+app.get('/farms', async(req, res) => {
+  const farms = await Farm.find({});
+  res.render('farms/index', { farms });
+})
+
+app.get('/farms/new', (req, res) => {
+  res.render('farms/new');
+})
+
+app.get('/farms/:id', async(req, res) => {
+  const farm = await Farm.findById(req.params.id).populate('products');
+  res.render('farms/show', { farm });
+})
+
+app.post('/farms', async(req, res) => {
+  const farm = new Farm(req.body);
+  await farm.save();
+  res.redirect('/farms');
+})
+
+app.get('/farms/:id/products/new', async(req, res) => {
+  const { id } = req.params;
+  const farm = await Farm.findById(id);
+  res.render('products/new', { categories, farm });
+})
+
+app.post('/farms/:id/products', async(req, res) => {
+  const { id } = req.params;
+  const farm = await Farm.findById(id);
+  const { name, price, category } = req.body;
+  const product = new Product({ name, price, category });
+  farm.products.push(product);
+  product.farm = farm;
+  await farm.save();
+  await product.save();
+  res.redirect(`/farms/${farm._id}`); // ${id} will work the same but this way we are just communicating that the id is coming from mongos db of farm.
+})
+
+app.delete('/farms/:id', async(req, res) => {
+  // const farm = await Farm.findByIdAndDelete(req.params.id);
+  res.redirect('/farms');
+})
+
+// PRODUCT ROUTES
 const categories = ['fruit', 'vegetable', 'dairy'];
 
 app.get('/products', async(req, res) => {
@@ -45,7 +92,7 @@ app.post('/products', async(req, res) => {
 
 app.get('/products/:id', async(req, res) => {
   const { id } = req.params;
-  const product = await Product.findById(id);
+  const product = await Product.findById(id).populate('farm', 'name');
   res.render('products/show', { product });
 })
 
